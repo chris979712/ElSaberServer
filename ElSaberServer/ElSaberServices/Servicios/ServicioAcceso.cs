00df102f046361c66d88dbf81d92fff487dbed01
Chris <chrisvasquez985@gmail.com>
2025-03-13 09:22:30 -0600

    Declaracion de servicios y métodos en el servicio de acceso

--- ElSaberServer/ElSaberServices/Servicios/ServicioAcceso.cs
+++ ElSaberServer/ElSaberServices/Servicios/ServicioAcceso.cs
@@ -11,6 +11,20 @@ namespace ElSaberServices.Servicios
 {
     public partial class ElSaberServices : IAccesoManejador
     {
+        public int ModificarContrasenia(string correo, string contrasenia)
+        {
+            AccesoOperaciones accesoOperaciones = new AccesoOperaciones();
+            int resultadoModificacion = accesoOperaciones.ModificarContrasenia(correo, contrasenia);
+            return resultadoModificacion;
+        }
+
+        public int VerificarCorreoExistente(string correo)
+        {
+            AccesoOperaciones accesoOperaciones = new AccesoOperaciones();
+            int resultadoVerificacion = accesoOperaciones.VerificarCorreoExistente(correo);
+            return resultadoVerificacion;
+        }
+
         public int VerificarCredenciales(string correo, string telefono)
         {
             AccesoOperaciones accesoOperaciones = new AccesoOperaciones();
